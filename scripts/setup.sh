959f3bce938d8bd720d031f0251addf9358f887f
Byunk <kyungho.byoun@gmail.com>
2023-11-18 18:47:43 +0900

    feat: backup main.cpp when setting up

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -3,9 +3,16 @@
 SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
 function setup () {
+	mkdir -p "$SCRIPTPATH/../backup"
+	mv "$SCRIPTPATH/../main.cpp" "$SCRIPTPATH/../backup/main.cpp"
+
 	cat > "$SCRIPTPATH/../main.cpp" << EOF
 #include <iostream>
 #include <vector>
+#include <queue>
+#include <algorithm>
+#include <string.h>
+#include <limits.h>
 	
 using namespace std;
 
